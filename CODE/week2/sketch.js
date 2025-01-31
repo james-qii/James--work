e28adac6aab458cadb3bc7e46e8a3075d1e2b92a
JAMESQI <230111849@qq.com>
2025-01-30 19:29:51 -0500

    css practice

--- CODE/week2/sketch.js
+++ CODE/week2/sketch.js
@@ -1,7 +1,12 @@
 function setup() {
     createCanvas(400, 400);
+  btn = select('#my-button'); 
+  btn.mouseClicked(onBtnClicked)
   }
   
+  function onBtnClicked(){
+    fill(random(255));
+  }
   function draw() {
     background(220);
 
